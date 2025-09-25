1ae44ebef688472d48a7781aa8543d003353d7b5
LilianJanicot <lilian.janicot@centrale-med.fr>
2025-09-25 22:20:52 +0200

    :construction: Correctif Story
    Commentaire sur le code non testé

--- gril/story_grille.py
+++ gril/story_grille.py
@@ -1,9 +1,8 @@
-from grille import Grille
+#from grille import Grille
 
-maGrille=Grille(5,8)
-while 1!=0:
-    maGrille.afficher()
-    entree = input("Entrez les coordonées séparées par un espace : ")
-    x, y = map(int, entree.split())
-    maGrille.tirer(x,y)
-
+#maGrille=Grille(5,8)
+#while 1!=0:
+#    maGrille.afficher()
+#    entree = input("Entrez les coordonées séparées par un espace : ")
+#    x, y = map(int, entree.split())
+#    maGrille.tirer(x,y)

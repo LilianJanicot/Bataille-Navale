1142ac3a6955461e2561da6abefdedd0ec75fb6d
LilianJanicot <lilian.janicot@centrale-med.fr>
2025-09-26 16:53:07 +0200

    🚑 Réparation des tests
    Nouveau test : test_ajoute
    Réparation de test_grille et test_bateau

--- boat/test_bateau.py
+++ boat/test_bateau.py
@@ -12,5 +12,5 @@ def test_init():
     assert monBateau.vertical==True
 
 def test_positions():
-    assert Bateau(2, 3, longueur=3).positions()==[(2, 3), (2, 4), (2, 5)]
-    assert Bateau(2, 3, longueur=3, vertical=True).positions() == [(2, 3), (3, 3), (4, 3)]
+    assert Bateau(2, 3, longueur=3).positions==[(2, 3), (2, 4), (2, 5)]
+    assert Bateau(2, 3, longueur=3, vertical=True).positions == [(2, 3), (3, 3), (4, 3)]
--- gril/test_grille.py
+++ gril/test_grille.py
@@ -1,4 +1,10 @@
 from grille import Grille
+#from ..boat.bateau import Bateau
+import sys
+from pathlib import Path
+sys.path.append(str(Path(__file__).parent.parent))
+from boat.bateau import Bateau
+
 
 def test_init():
     maGrille=Grille(3,7)
@@ -15,3 +21,9 @@ def test_tirer():
 def test_afficher():
     maGrille=Grille(2,3)
     assert str(maGrille)=="~~~\n~~~\n"
+
+def test_ajoute():
+    maGrille=Grille(2,4)
+    monBateau1=Bateau(0,1,2,True)
+    maGrille.ajoute(monBateau1)
+    assert str(maGrille)=="~⛵~~\n~⛵~~\n"
